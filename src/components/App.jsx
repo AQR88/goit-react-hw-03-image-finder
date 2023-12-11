66e9c0aab758ca0f823f8286354bdcfa7df08eeb
AQR88 <timahaua@gmail.com>
2023-12-11 02:42:13 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import * as ImageService from './Services/Api';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
 import { ImageGallery } from './ImageGallery/ImageGallery';
-import { Button } from './Button/Button.jsx';
+import { Button } from 'components/Button/Button';
 import { FidgetSpinner } from 'react-loader-spinner';
 
 class App extends Component {
